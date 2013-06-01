2f6e60ac1bbceca674715807a93499ef1fd14539
Mispy <^_^@mispy.me>
2013-06-02 06:01:23 +1000

    Example works with higher-level console wrapper

--- lib/libtcod/console.rb
+++ lib/libtcod/console.rb
@@ -3,7 +3,7 @@ module TCOD
     attr_accessor :width, :height
 
     def self.root
-      @root ||= TCOD.nsole.new(0, 0, true)
+      @root ||= TCOD::Console.new(0, 0, true)
     end
 
     def initialize(w, h, root=False)
@@ -16,31 +16,38 @@ module TCOD
       end
     end
 
-    def self.init_root(width, height, title, fullscreen=false, renderer=RENDERER_SDL)
+    def init_root(width, height, title, fullscreen=false, renderer=RENDERER_SDL)
       TCOD.console_init_root(width, height, title, fullscreen, renderer)
-      root.width = width
-      root.height = height
-      root
+      Console.root.width = width
+      Console.root.height = height
     end
 
-    def window_closed?
-      TCOD.console_is_window_closed
+    def set_window_title(title); TCOD.console_set_window_title(title); end
+    def set_fullscreen(bool); TCOD.console_set_fullscreen(bool); end
+    def is_fullscreen?; TCOD.console_is_fullscreen; end
+    def window_closed?; TCOD.console_is_window_closed; end
+    def set_custom_font(fontFile, flags, nb_char_horiz=0, nb_char_vertic=0)
+      TCOD.console_set_custom_font(fontFile, flags, nb_char_horiz, nb_char_vertic)
+    end
+    def map_ascii_code_to_font(asciiCode, fontCharX, fontCharY)
+      TCOD.console_map_ascii_code_to_font(asciiCode.ord, fontCharX, fontCharY)
+    end
+    def map_ascii_codes_to_font(asciiCode, nbCodes, fontCharX, fontCharY)
+      TCOD.console_map_ascii_code_to_font(asciiCode.ord, nbCodes, fontCharX, fontCharY)
     end
 
-    def set_default_foreground(color)
-      TCOD.console_set_default_foreground(@ptr, color)
-    end
+
+    def set_default_background(color); TCOD.console_set_default_background(@ptr, color); end
+    def set_default_foreground(color); TCOD.console_set_default_foreground(@ptr, color); end
+    def clear; TCOD.console_clear(@ptr); end
 
     def put_char(x, y, c, flag=BKGND_DEFAULT)
       TCOD.console_put_char(@ptr, x, y, c.ord, flag)
     end
 
-    def flush
-      TCOD.console_flush
-    end
+    def flush; TCOD.console_flush; end
 
-    def wait_for_keypress(flush=false)
-      TCOD.console_wait_for_keypress(flush)
-    end
+    def wait_for_keypress(flush=false); TCOD.console_wait_for_keypress(flush); end
+    def key_pressed?(keycode); TCOD.console_is_key_pressed(keycode); end
   end
 end
