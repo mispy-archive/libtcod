205b188c3d3c92a989028a30d77e6a199163d475
Remco Peereboom <rpeereboom@gmail.com>
2015-07-02 02:12:19 +0200

    Moved system behaviour from Console to System.
    Moved several methods that had nothing to do with being a rectanlge of
    cells from the console class to the system module.
    
    Added some attribute accessors to the system module (e.g. title and
    fullscreen).
    
    Changed some of the formatting to be more compliant to the community
    style guide.

--- lib/libtcod/system.rb
+++ lib/libtcod/system.rb
@@ -34,5 +34,51 @@ module TCOD
       @title = new_title
       TCOD.console_set_window_title(new_title)
     end
+
+    def self.fullscreen?
+      @fullscreen
+    end
+
+    def self.to_fullscreen
+      @fullscreen = true
+      TCOD.console_set_fullscreen(true)
+    end
+
+    def self.to_windowed
+      @fullscreen = false
+      TCOD.console_set_fullscreen(false)
+    end
+
+    def self.window_closed?
+      TCOD.console_is_window_closed
+    end
+
+    def self.flush
+      TCOD.console_flush
+    end
+
+    def self.check_for_keypress(flags = TCOD::KEY_PRESSED)
+      TCOD.console_check_for_keypress(flags)
+    end
+
+    def self.wait_for_keypress(flush = false)
+      TCOD.console_wait_for_keypress(flush)
+    end
+
+    def self.key_pressed?(keycode)
+      TCOD.console_is_key_pressed(keycode)
+    end
+
+    def self.set_custom_font(font_file, flags, columns = 0, rows = 0)
+      TCOD.console_set_custom_font(font_file, flags, columns, rows)
+    end
+
+    def self.map_ascii_code_to_font(ascii_code, coord)
+      TCOD.console_map_ascii_code_to_font(ascii_code.ord, coord.x, coord.y)
+    end
+
+    def self.map_ascii_codes_to_font(ascii_code, n, offset)
+      TCOD.console_map_ascii_code_to_font(ascii_code.ord, n, offset.x, offset.y)
+    end
   end
 end
